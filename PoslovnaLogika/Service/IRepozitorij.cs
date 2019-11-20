f1fb3f49feed827a42f8be2da2402b7e236bfd50
Jure <jureperic@gmail.com>
2019-11-20 10:40:49 +0100

    repozitorij

---
+++ PoslovnaLogika/Service/IRepozitorij.cs
@@ -0,0 +1,19 @@
+﻿using PoslovnaLogika.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoslovnaLogika.Service
+{
+    public interface IRepozitorij<TEntity> where TEntity : class
+    {
+        TEntity Dohvati(int id);
+        IEnumerable<TEntity> DohvatiSve();
+        
+        void Krairaj(TEntity objekt);
+        void Uredi(TEntity objekt);
+        void Izbrisi(TEntity objekt);        
+    }
+}
---
+++ PoslovnaLogika/Service/Repozitorij.cs
@@ -0,0 +1,42 @@
+﻿using PoslovnaLogika.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoslovnaLogika.Service
+{
+    class Repozitorij<TEntity> : IRepozitorij<TEntity> where TEntity : class
+    {
+        protected readonly DbContext Context;
+
+        public Repozitorij(DbContext context)
+        {
+            Context = context;
+        }
+
+        public TEntity Dohvati(int id)
+        {
+            return Context.Set<TEntity>().Find(id);
+        }
+        public IEnumerable<TEntity> DohvatiSve()
+        {
+            return Context.Set<TEntity>();
+        }
+        
+        public void Krairaj(TEntity objekt)
+        {
+            Context.Set<TEntity>().Add(objekt);
+        }
+        public void Uredi(TEntity objekt)
+        {
+            Context.Entry(objekt).State = EntityState.Modified;
+        }
+        public void Izbrisi(TEntity objekt)
+        {
+            Context.Set<TEntity>().Remove(objekt);
+        }
+    }
+}
